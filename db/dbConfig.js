a1396ca4dabc9562718925f4ccc050d18be8d343
Levickiy-hub <375293166089s@gmail.com>
2023-04-06 15:24:52 +0300

    fix:db config

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -12,13 +12,26 @@ const Organizations = sequelize.define(
            allowNull:false,
            primaryKey: true
        },
-        name: {type: Sequelize.STRING},
-        description: {type: Sequelize.STRING},
-        email: {type: Sequelize.STRING},
-        address: {type: Sequelize.STRING},
+        Clinic_name: {type: Sequelize.STRING},
+        Organization_name: {type: Sequelize.STRING},
+        Organization_number: {type: Sequelize.STRING},
+        Email: {type: Sequelize.STRING},
+        Address1: {type: Sequelize.STRING},
+        Address2: {type: Sequelize.STRING},
+        Postcode: {type: Sequelize.STRING},
+        Post_town: {type: Sequelize.STRING},
         phone: {type: Sequelize.STRING},
-        type: {type: Sequelize.STRING},
-        LogSystemType: {type: Sequelize.STRING}
+        Billing_address1: {type: Sequelize.STRING},
+        Billing_address2: {type: Sequelize.STRING},
+        Billing_postcode: {type: Sequelize.STRING},
+        Billing_post_town: {type: Sequelize.STRING},
+        Billing_email: {type: Sequelize.STRING},
+        Phone: {type: Sequelize.STRING},
+        Mob_number: {type: Sequelize.STRING},
+        Type: {type: Sequelize.STRING},
+        Payment_plan: {type: Sequelize.STRING},
+        Journal_system: {type: Sequelize.STRING},
+        Xray_system: {type: Sequelize.STRING},
     },
     {
         // Здесь определяются другие настройки модели
@@ -34,13 +47,18 @@ const Users = sequelize.define(
             allowNull:false,
             primaryKey: true
         },
-        name: {type: Sequelize.STRING},
-        bankId: {type: Sequelize.STRING},
-        email: {type: Sequelize.STRING},
-        address: {type: Sequelize.STRING},
-        phone: {type: Sequelize.STRING},
-        role: {type: Sequelize.STRING},
-        organizationId: {type: Sequelize.STRING}
+        First_name: {type: Sequelize.STRING},
+        Last_name: {type: Sequelize.STRING},
+        Personal_number: {type: Sequelize.STRING},
+        Mob_number: {type: Sequelize.STRING},
+        Address1: {type: Sequelize.STRING},
+        Address2: {type: Sequelize.STRING},
+        Postcode: {type: Sequelize.STRING},
+        Post_town: {type: Sequelize.STRING},
+        Email: {type: Sequelize.STRING},
+        Role: {type: Sequelize.STRING},
+        Position: {type: Sequelize.STRING},
+        OrganizationID: {type: Sequelize.INTEGER}
 
     },
     {
@@ -48,45 +66,70 @@ const Users = sequelize.define(
     }
 )
 Users.sync({alter: true}).then(r =>console.log('Users Create')).catch(err=>console.error(err.message))
-const Requests = sequelize.define(
-    'Requests',
+const Orders = sequelize.define(
+    'Orders',
     {
         id:{type: Sequelize.INTEGER,
             autoIncrement:true,
             allowNull:false,
             primaryKey: true
         },
-        userId: {type: Sequelize.INTEGER},
-        organizationId: {type: Sequelize.INTEGER},
-        whom: {type: Sequelize.INTEGER},
-        text: {type: Sequelize.STRING},
-        description: {type: Sequelize.STRING},
-        alertType: {type: Sequelize.STRING},
-        status: {type: Sequelize.STRING},
+        Creator_UserID: {type: Sequelize.INTEGER},
+        OrganizationID: {type: Sequelize.INTEGER},
+        Creator_OrganizationID: {type: Sequelize.INTEGER},
+        PatientID: {type: Sequelize.INTEGER},
+        Permission_BankID: {type: Sequelize.STRING},
+        Permission_file: {type: Sequelize.STRING},
+        Order_docs_list: {type: Sequelize.STRING},
+        Order_Message: {type: Sequelize.STRING},
+        Alert_Type: {type: Sequelize.STRING},
+        Status: {type: Sequelize.STRING},
     },
     {
         // Здесь определяются другие настройки модели
     }
 )
-Requests.sync({alter: true}).then(r =>console.log('Requests Create')).catch(err=>console.error(err.message))
-const Answers = sequelize.define(
-    'Answers',
+Orders.sync({alter: true}).then(r =>console.log('Orders Create')).catch(err=>console.error(err.message))
+const Cases = sequelize.define(
+    'Cases',
     {
         id:{type: Sequelize.INTEGER,
             autoIncrement:true,
             allowNull:false,
             primaryKey: true
         },
-        requestId: {type: Sequelize.INTEGER},
-        text: {type: Sequelize.INTEGER},
-        file: {type: Sequelize.INTEGER},
-        description: {type: Sequelize.STRING},
+        OrderID: {type: Sequelize.INTEGER},
+        Files_pdf: {type: Sequelize.STRING},
+        Files_xray: {type: Sequelize.STRING},
+        Case_Message: {type: Sequelize.STRING},
+        UserID: {type: Sequelize.INTEGER},
     },
     {
         // Здесь определяются другие настройки модели
     }
 )
-Answers.sync({alter: true}).then(r =>console.log('Answers Create')).catch(err=>console.error(err.message))
+Cases.sync({alter: true}).then(r =>console.log('Cases Create')).catch(err=>console.error(err.message))
+const Resend = sequelize.define(
+    'Resend',
+    {
+        id:{type: Sequelize.INTEGER,
+            autoIncrement:true,
+            allowNull:false,
+            primaryKey: true
+        },
+        CasesID: {type: Sequelize.INTEGER},
+        Resend_email: {type: Sequelize.STRING},
+        Resend_name: {type: Sequelize.STRING},
+        Resend_organization: {type: Sequelize.STRING},
+        Resend_message: {type: Sequelize.STRING},
+        Recipient_UserID: {type: Sequelize.STRING},
+        UserID: {type: Sequelize.INTEGER},
+    },
+    {
+        // Здесь определяются другие настройки модели
+    }
+)
+Resend.sync({alter: true}).then(r =>console.log('Resend Create')).catch(err=>console.error(err.message))
 module.exports = {
-    sequelize, Users,Organizations,Requests,Answers
+    sequelize, Users,Organizations,Orders,Cases,Resend
 }
